63b9f5d70995884154f6afb5038edee02155a993
Umar. Mansyur <umar.ovie@gmail.com>
2022-08-24 15:14:07 +0700

    Ini ges

--- src/routes/pubsub.ts
+++ src/routes/pubsub.ts
@@ -1,37 +1,17 @@
 import express, { Request, Response, NextFunction } from "express";
 import Db from "../libs/db";
-import mqtt from 'mqtt';
+import { client } from "../libs/pubsub";
 const router = express.Router();
 
 
-router.get('/', function(req:Request, res: Response, next: NextFunction) {
+router.get('/', function (req: Request, res: Response, next: NextFunction) {
     return new Promise<any>(async (resolve, reject) => {
         try {
-            const host = 'broker.emqx.io';
-            const port = '1883';
-            const clientId = `mqtt_${Math.random().toString(16).slice(3)}`;
-
-
-            const connectUrl = `mqtt://${host}:${port}`;
-            const client = mqtt.connect(connectUrl, {
-                clientId,
-                clean: true,
-                connectTimeout: 4000,
-                username: 'emqx',
-                password: 'public',
-                reconnectPeriod: 1000,
-            });
-
-            const topic = '/nodejs/mqtt';
+            const topic = 'air/tinggi';
             client.on('connect', () => {
                 console.log('Connected');
                 client.subscribe([topic], () => {
                     console.log(`Subscribe to topic ${topic}`)
-                });
-                client.publish(topic, 'Ali', { qos: 0, retain: false }, (error) => {
-                    if (error) {
-                        console.log(error);
-                    }
                 });
                 client.on('message', (topic, payload) => {
                     console.log('Received Message:', topic, payload.toString())
@@ -43,6 +23,26 @@ router.get('/', function(req:Request, res: Response, next: NextFunction) {
         }
     })
 });
+router.post('/', (req: Request, res: Response) => {
+    return new Promise((resolve, reject) => {
+        try {
+            const topic = 'air/tinggi';
+            client.on('connect', () => {
+                console.log('Connected');
+                client.publish(topic, req.body.text, { qos: 0, retain: false }, (error) => {
+                    if (error) {
+                        console.log(error);
+                    }
+                });
+                client.on('message', (topic, payload) => {
+                    console.log('Received Message:', topic, payload.toString())
+                })
+            });
+            resolve(res.send('Data published successfully'))
+        } catch (error) {
 
+        }
+    })
+});
 
 export default router
