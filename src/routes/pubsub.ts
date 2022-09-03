3f798f3925a63f9a8bb38b1f764dcc8e0b961ffa
AliRahman06 <4li.ar45@gmail.com>
2022-09-03 10:15:07 +0700

    ubahan pubsub

--- src/routes/pubsub.ts
+++ src/routes/pubsub.ts
@@ -1,88 +1,36 @@
 import express, { Request, Response, NextFunction } from "express";
-import Db from "../libs/db";
 import { client } from "../libs/pubsub";
 const router = express.Router();
 
 
-router.get('/air', function (req: Request, res: Response, next: NextFunction) {
-    return new Promise<any>(async (resolve, reject) => {
-        try {
-            const topic = 'air/tinggi';
-            client.on('connect', () => {
-                console.log('Connected');
-                client.subscribe([topic], () => {
-                    console.log(`Subscribe to topic ${topic}`)
-                });
-                client.on('message', (topic, payload:any) => {
-                    const nilai = JSON.parse(payload);
-                    console.log('Received Message:', topic, payload.toString());
-                    Db.query('INSERT INTO tbl_air VALUES (NULL,?,NOW())',[nilai.tinggiAir]);
-                });
-            });
-            resolve(res.send('Data Subscribe successfully'))
-        } catch (error) {
-            reject(error);
-        }
-    })
-});
-router.get('/nutrisi', function (req: Request, res: Response, next: NextFunction) {
-    return new Promise<any>(async (resolve, reject) => {
-        try {
-            const topic = 'nutrisi/tinggi';
-            client.on('connect', () => {
-                console.log('Connected');
-                client.subscribe([topic], () => {
-                    console.log(`Subscribe to topic ${topic}`)
-                });
-                client.on('message', (topic, payload) => {
-                    const nilai = JSON.parse(req.body.payload);
-                    console.log('Received Message:', topic, payload.toString());
-                    Db.query('INSERT INTO tbl_nutrisi VALUES (NULL,?,NOW())',[nilai.tinggiAir]);
-                });
-            });
-            resolve(res.send('Data Subscribe successfully'))
-        } catch (error) {
-            reject(error);
-        }
-    })
-});
 router.post('/air', (req: Request, res: Response) => {
     return new Promise((resolve, reject) => {
         try {
-            const topic = 'air/tinggi';
-            client.on('connect', () => {
-                console.log('Connected');
-                client.publish(topic, req.body.text, { qos: 0, retain: false }, (error) => {
-                    if (error) {
-                        console.log(error);
-                    }
-                });
-                client.on('message', (topic, payload) => {
-                    console.log('Received Message:', topic, payload.toString())
-                })
+            const input = req.body.text;
+            console.log(input)
+            const topic = 'melon/siram';
+            client.publish(topic, input, { qos: 0, retain: false }, (error) => {
+                if (error) {
+                    console.log(error);
+                }   
             });
-            resolve(res.send('Data published successfully'))
+            resolve( res.json({message: 'Publish Berhasil'}));
         } catch (error) {
-
+            reject(error);
         }
     })
 });
 router.post('/nutrisi', (req: Request, res: Response) => {
     return new Promise((resolve, reject) => {
         try {
-            const topic = 'air/tinggi';
-            client.on('connect', () => {
-                console.log('Connected');
-                client.publish(topic, req.body.text, { qos: 0, retain: false }, (error) => {
-                    if (error) {
-                        console.log(error);
-                    }
-                });
-                client.on('message', (topic, payload) => {
-                    console.log('Received Message:', topic, payload.toString())
-                })
+            const topic = 'melon/siram';
+            const input = req.body.text;  
+            client.publish(topic, input, { qos: 0, retain: false }, (error) => {
+                if (error) {
+                    console.log(error);
+                } 
             });
-            resolve(res.send('Data published successfully'))
+            resolve( res.json({message: 'Publish Berhasil'}));
         } catch (error) {
 
         }
